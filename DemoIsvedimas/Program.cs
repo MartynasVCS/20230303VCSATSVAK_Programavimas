36f37aaf1b4034ca13b28cf7b518cc2cfb2b6307
Martynas Satas <m.satas@gmail.com>
2023-03-27 21:34:35 +0300

    Add demo code for variables and outputing to console

---
+++ DemoIsvedimas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DemoIsvedimas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sveikas pasauli!");
+            Console.WriteLine();
+
+            // Kintamųjų išvedimas
+            int amzius = 25;
+            float ugis = 2.15f;
+            Console.WriteLine("amzius");
+            Console.WriteLine(amzius);
+            Console.WriteLine();
+
+            Console.WriteLine("Labas " + "vakaras");
+            Console.WriteLine("Mano amžius: " + amzius + ", o mano ūgis: " + ugis);
+            Console.WriteLine("Mano amžius: {0}, o mano ūgis: {1}", amzius, ugis);
+            Console.WriteLine($"Mano amžius: {amzius}, o mano ūgis: {ugis}");
+
+            Console.Write("Labas");
+            Console.WriteLine("Labas");
+            Console.Write("Labas");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DemoKintamieji/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DemoKintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Čia rašysime kodą
+
+            /*
+                Komentaras
+                per
+                kelias
+                eilutes
+             */
+
+            // Sveikieji
+            int skaicius = 5;
+            int amzius = skaicius;
+
+            // Trupmeniniai
+            float ugis = 1.85f;
+            float bekablelis = 4f;
+            double tikslesnis = 1.123456789123456;
+
+            // Simboliniai
+            char simbolis = 'a';
+            simbolis = 'b';
+            simbolis = 'c';
+            char kitasSimbolis = simbolis;
+            string tekstas = "gdhuaslg ajghds jkkjawdsgh 5645ar gęė4čšįųnįšųū(į%#&@%^@#*&^%@#";
+
+            // Loginiai
+            bool arLyja = false;
+            bool arTamsu = true;
+        }
+    }
+}
