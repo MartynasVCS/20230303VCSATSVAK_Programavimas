fff66bf70f5cfd601c8d18af1814cde5e97a39ad
MartynasVCS <martynasvcs@gmail.com>
2023-03-30 19:05:37 +0300

    Add solution for homework 1

---
+++ Uzduotis01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Uzduotis01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // 1 punktas
+            string name = "Martynas";
+            Console.WriteLine("Labas " + name);
+            Console.WriteLine();
+
+            // 2 punktas
+            int age = 34;
+            Console.WriteLine("Ivestas amzius: " + age);
+            Console.WriteLine();
+
+            // 3 punktas
+            int number = 25;
+            // Pirmas budas
+            Console.Write(number);
+            Console.Write(number);
+            Console.Write(number);
+            Console.Write(number);
+            Console.WriteLine(number);
+
+            // Antras budas
+            Console.WriteLine("{0}{1}{2}{3}{4}", number, number, number, number, number);
+
+            // Trecias budas
+            Console.WriteLine("{0}{0}{0}{0}{0}", number);
+            Console.WriteLine();
+
+            // Ketvirtas būdas
+            Console.WriteLine($"{number}{number}{number}{number}{number}");
+            Console.WriteLine();
+
+            // 4 punktas
+            Console.WriteLine("{0} {0} {0} {0} {0}", number);
+
+            // Kitas būdas
+            Console.WriteLine($"{number} {number} {number} {number} {number}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Uzduotis02/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Uzduotis02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Paprastas budas
+            string startLine = "---------------------";
+            string emptyLine = "|                   |";
+            string name = "Martynas";
+            Console.WriteLine(startLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine("|    Mano vardas    |");
+            Console.WriteLine("|      " + name + "     |");
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(startLine);
+            Console.WriteLine();
+
+            // Sudetingesnis budas (naudojami for ciklai)
+            int eilutesIlgis = 25;
+            string pasisveikinimas = "Sveiki, mano vardas yra";
+            string vardas = "Martynas";
+            PradziosPabaigosEilute(eilutesIlgis);
+            TusciaEilute(eilutesIlgis);
+            TusciaEilute(eilutesIlgis);
+            EiluteSuTekstu(eilutesIlgis, pasisveikinimas);
+            EiluteSuTekstu(eilutesIlgis, vardas);
+            TusciaEilute(eilutesIlgis);
+            TusciaEilute(eilutesIlgis);
+            PradziosPabaigosEilute(eilutesIlgis);
+        }
+
+        // Metodas atspausdinantis tuscia eilute pagal uzduota ilgi
+        internal static void TusciaEilute(int eilutesIlgis)
+        {
+            Console.Write("|");
+            for (int i = 0; i < eilutesIlgis; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("|");
+        }
+
+        // Metodas atspausdinantis pradzios/pabaigos eilute pagal uzduota ilgi
+        internal static void PradziosPabaigosEilute(int eilutesIlgis)
+        {
+            for (int i = 0; i < eilutesIlgis + 2; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+
+        // Metodas atspausdinantis eilute su tekstu
+        internal static void EiluteSuTekstu(int ilgis, string tekstas)
+        {
+            int abiejuTarpuIlgis = ilgis - tekstas.Length;
+            int tarpasKaireje = abiejuTarpuIlgis / 2;
+            int tarpasDesineje = abiejuTarpuIlgis / 2 + abiejuTarpuIlgis % 2;
+
+            Console.Write("|");
+            for (int i = 0; i < tarpasKaireje; i++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write(tekstas);
+
+            for (int i = 0; i < tarpasDesineje; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("|");
+        }
+    }
+}
