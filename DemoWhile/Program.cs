eec497357a2fbdfd9c31862e59df2bfc9482a7cb
MartynasVCS <martynasvcs@gmail.com>
2023-04-03 20:59:24 +0300

    Add demo for while cycle

---
+++ DemoWhile/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DemoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lietuviškų raidžių rodymas console lange
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            int skaicius = 5;
+            int i = 0;
+
+            // For atitikmuo užrašytas kaip while ciklas
+            while (i < 11)
+            {
+                Console.WriteLine($"{skaicius} * {i} = {skaicius * i}");
+                i++;
+            }
+            Console.WriteLine();
+
+            i = 0;
+            while (true)
+            {
+                Console.WriteLine("Kas čia vyksta?!?");
+                i++;
+
+                if (i > 10)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+
+            // Įvedimas su duomenų tipo apsauga
+            int ivedimas;
+            while (true)
+            {
+                Console.Write("Įveskite skaičių: ");
+
+                try
+                {
+                    ivedimas = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"Įvedėte ne skaičių, bandykite dar kartą.");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine($"Įvedėte per didelį skaičių, bandykite dar kartą.");
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            Console.WriteLine($"Įvestas skaičius: {ivedimas}");
+        }
+    }
+}
